2a0e68d6dabea2fa394c420a8e409684d1185de8
Christian Hapgood <christian.hapgood@gmail.com>
2019-12-08 00:24:41 -0400

    Lesson 40 embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email      string
+	postalCode string
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Johnson",
+		contact: contactInfo{
+			email:      "jim.johnson@gmail.com",
+			postalCode: "E3S1B5",
+		},
+	}
 
-	fmt.Println(alex)
+	fmt.Printf("%+v", jim)
 }
